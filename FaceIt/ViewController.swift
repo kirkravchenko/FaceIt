f819eef266b18f20ab1584b4c846d9007c5599f5
kirkravchenko <kyrylkravchenko1994@gmail.com>
2021-12-13 21:06:12 +0200

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Kyrylo Kravchenko on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
